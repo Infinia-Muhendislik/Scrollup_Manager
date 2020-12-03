317a8fbbfa4e3815f820d4620f490b80a30b192d
Mert Gül <ibrahimmertgul@gmail.com>
2020-12-03 09:35:47 +0300

    player starter position changed

--- manager.sh
+++ manager.sh
@@ -1,10 +1,10 @@
 sleep 2.99
 cd /home/pi/Scrollup_Manager/ # go folder
+chmod +x player.sh
+lxterminal -e "./player.sh"
 git fetch --all # check update
 git reset --hard origin/master # pull update and ignore stash
 npm install # install dependecies
-chmod +x player.sh
-lxterminal -e "./player.sh"
 npm start # run player
 npm start # run again if something fail on player
 npm start # run again if something fail on player
