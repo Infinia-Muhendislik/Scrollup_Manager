843f0bfbddc75b24c0a7acc38778a1deb0fa9938
Mert Gül <ibrahimmertgul@gmail.com>
2020-12-04 10:46:56 +0300

    env gitignored

--- player.sh
+++ player.sh
@@ -1,13 +1,8 @@
-cd /home/pi/remote_media_player/ #
-echo go folder
-git fetch --all #
-echo check update
-git reset --hard origin/master # 
-echo pull update and ignore stash
-npm install #
-echo install dependecies
-npm start #
-echo run player
+cd /home/pi/remote_media_player/ # go folder
+git fetch --all # check update
+git reset --hard origin/master # pull update and ignore stash
+npm install # install dependecies
+npm start # run player
 npm start # run again if something fail on player
 npm start # run again if something fail on player
 npm start # run again if something fail on player
