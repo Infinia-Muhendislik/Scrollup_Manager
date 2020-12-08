4d49a0a6585db31c8fc5c8c3d125e5329042c0a8
Mert Gül <ibrahimmertgul@gmail.com>
2020-12-08 10:17:37 +0300

    Update manager.sh

--- manager.sh
+++ manager.sh
@@ -1,7 +1,7 @@
-sleep 2.99
 cd /home/pi/Scrollup_Manager/ # go folder
 chmod +x player.sh
 lxterminal -e "./player.sh"
+sleep 2.99
 git fetch --all # check update
 git reset --hard origin/master # pull update and ignore stash
 npm install # install dependecies
